a948158f574c6f63d66862329251b6c67fb6e34b
Christopher Steel <chris.steel@gmail.com>
2010-06-12 22:07:12 -0400

    made version 1 of horizontal 1 menu using helpers

--- controllers/aria.py
+++ controllers/aria.py
@@ -5,3 +5,7 @@ def index(): return dict(message="hello from aria.py index")
 # menu samples from 
 # http://test.cita.illinois.edu/aria/nav/nav1.html
 def menu_examples(): return dict(message="hello from aria.py index")
+
+def menu_horizontal_1(): return dict(message="hello from aria.py index")
+
+def menu_horizontal_2(): return dict(message="hello from aria.py index")
